e123924b2f31b4dacb45050310a0c0e8e37f44e5
leonardrua <leonardrua8@gmail.com>
2025-07-01 08:08:44 +0800

    C

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,15 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y waydroid 
 
 # Use a COPR Example:
 #
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+dnf5 -y copr enable cuteneko/waydroid-helper
+dnf5 -y install waydroid-helper
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable cuteneko/waydroid-helper
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
