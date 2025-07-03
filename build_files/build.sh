f59681ea15a4c1c670a20fdaaf09de6600ac06dd
leonardrua <101442512+leonardrua@users.noreply.github.com>
2025-07-03 23:14:14 +0800

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -25,7 +25,9 @@ for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-
     rpm --erase $pkg --nodeps
 done
 
+mkdir -p "/var/opt" && ln -s "/var/opt" "/opt"
 dnf5 -y install vivaldi-stable
+mkdir -p "/usr/lib/opt/vivaldi" && ln -s "/usr/lib/opt/vivaldi" "/var/opt/vivaldi"
 dnf5 -y install waydroid-helper
 dnf5 -y install ananicy-cpp
 dnf5 -y install cachyos-ksm-settings
