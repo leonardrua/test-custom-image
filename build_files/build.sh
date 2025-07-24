1b7525bd86f50f85982c66cdf6e2e9ab81ca56cb
Leonard Rua <leonardrua8@gmail.com>
2025-07-24 09:27:18 +0800

    V

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,16 +13,30 @@ set -ouex pipefail
 dnf5 install -y waydroid 
 
 # Use a COPR Example:
-#
 dnf5 -y copr enable cuteneko/waydroid-helper
 dnf5 -y config-manager addrepo --from-repofile=https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo
+dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
+dnf5 -y copr enable bieszczaders/kernel-cachyos
+
+dnf5 -y install kernel-cachyos kernel-cachyos-devel-matched
+
+
+for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra; do
+    rpm --erase $pkg --nodeps
+done
 
 dnf5 -y install waydroid-helper
 mkdir -p "/var/opt" && ln -s "/var/opt" "/opt"
 mkdir -p "/usr/lib/cloudflare-warp" && ln -s "/usr/lib/cloudflare-warp" "/var/opt/cloudflare-warp"
 dnf5 -y install cloudflare-warp
+dnf5 -y install ananicy-cpp
+dnf5 -y install cachyos-ksm-settings
+dnf5 -y install cachyos-settings --allowerasing
 # Disable COPRs so they don't end up enabled on the final image:
 dnf5 -y copr disable cuteneko/waydroid-helper
 
+KERNEL="$(rpm -qa | grep -P 'kernel-(|'"cachyos"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"cachyos"'-)//')"
+/usr/bin/dracut --no-hostonly --kver "$KERNEL" --reproducible -v --add ostree -f "/lib/modules/$KERNEL/initramfs.img"
+chmod 0600 "/lib/modules/$KERNEL/initramfs.img"
 
 alternatives --set fakeroot /usr/bin/fakeroot-sysv
